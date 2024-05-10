64cd9bef6e75db5b3f1f131b23844323430525a6
lopesmathe <matheuslopesbauer@gmail.com>
2024-05-10 11:07:02 -0300

    add projeto 3

---
+++ Projeto 3/src/br/edu/up/controles/ControleEstande.java
@@ -0,0 +1,17 @@
+package br.edu.up.controles;
+
+import br.edu.up.modelos.Livro;
+
+public class ControleEstande {
+
+    private Livro[] livros;
+
+    public Livro[] getLivros() {
+        return livros;
+    }
+
+    public void setLivros(Livro[] livros) {
+        this.livros = livros;
+    }
+
+}
