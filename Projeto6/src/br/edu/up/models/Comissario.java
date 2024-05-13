a460c8bf4cc421218c6fc7277fd53e6fbe6f1a61
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-05-13 09:31:22 -0300

    Projeto 6 setup dos modelos

---
+++ Projeto6/src/br/edu/up/models/Comissario.java
@@ -0,0 +1,29 @@
+package br.edu.up.models;
+import java.util.ArrayList;
+public class Comissario extends Tripulacao{
+    private ArrayList<String> idiomas;
+
+    public Comissario(String nome, int rg, int idAeronauticam, int matricula,String[] idiomas, Aeronave aeronave)
+    {
+        super.setNome(nome);
+        super.setRg(rg);
+        super.setAeronave(aeronave);
+        //---Pessoa
+        super.setIdAeronauticam(idAeronauticam);
+        super.setMatricula(matricula);
+        //---Tripulação
+        for(int i =0; i< idiomas.length; i++){
+            this.idiomas.add(idiomas[i]);
+        }
+        //---Idiomas
+        //Adiciona o array[] ao ArrayList<> 
+    }
+
+    //Pessoas podem aprender um idioma novo, então pode ser preciso fazer a adição de novos idiomas
+    public void addIdioma(String idiomas){
+        this.idiomas.add(idiomas);
+    }
+    public void removeIdioma(String idiomas){
+        this.idiomas.remove(idiomas);
+    }
+}
---
+++ Projeto6/src/br/edu/up/view/Menu.java
@@ -0,0 +1,7 @@
+package br.edu.up.view;
+
+public class Menu {
+    // Um aeroporto precisa-se registrar informações sobre as diferentes pessoas que nele trafegam.
+    // criar um programa que permita ao usuário realizar as operações necessários para o controle do tráfego.
+    // isso inclui, cadastrar 1 voo, quantos pessoas vão no avião? a tripulação e os passageiros. Pelo menos 1 de cada
+}
