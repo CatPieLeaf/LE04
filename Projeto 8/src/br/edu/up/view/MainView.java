c8312d566aca2c61d92247b779577653beb54d8b
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-05-16 12:04:50 -0300

    Project 8 not finished

--- Projeto 8/src/br/edu/up/view/MainView.java
+++ Projeto 8/src/br/edu/up/view/MainView.java
@@ -11,6 +11,7 @@ public class MainView {
     public MainView(){
         run();
     }
+    //Fazer algo decente, fazer um programa CRUD
     public void run() {
         Comercial comercio1 = new Comercial(01, "PETS LTDA", "(41) 99147-2421", "142.410-40/0001" );
         Comercial comercio2 = new Comercial(02, "hamburguer LTDA", "(41) 99234-2551", "159.305-08/0001" );
