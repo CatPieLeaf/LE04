5611d8a3c56fe6c1c27732a66201f5392bcb5264
lopesmathe <matheuslopesbauer@gmail.com>
2024-05-20 10:39:44 -0300

    Projeto 4 Parcial

--- Projeto 4/src/br/edu/up/controles/ControleDeCarro.java
+++ Projeto 4/src/br/edu/up/controles/ControleDeCarro.java
@@ -5,4 +5,8 @@ import br.edu.up.modelos.*;
 public class ControleDeCarro {
 
     ModeloDeCarro[] modelo = new ModeloDeCarro[10];
+
+    public ControleDeCarro(ModeloDeCarro[] modelo) {
+        this.modelo = modelo;
+    }
 }
--- Projeto 4/src/br/edu/up/telas/Menu.java
+++ Projeto 4/src/br/edu/up/telas/Menu.java
@@ -7,29 +7,49 @@ import br.edu.up.modelos.*;
 import java.util.Scanner;
 
 public class Menu {
-    public static void run() {
-        System.out.println("1: colocar carro");
-        System.out.println("2: tirar carro");
-        Scanner scanner = new Scanner(System.in);
-        int escolha = scanner.nextInt();
-        switch (escolha) {
-            case 1:
-                colocarCarro();
-                break;
+    private ControleDeCarro controle;
+    private ModeloDeCarro[] modelo = new ModeloDeCarro[10];
+    private Scanner scanner = new Scanner(System.in);
 
-            default:
-                break;
+    public void run() {
+        while (true) {
+
+            System.out.println("1: colocar carro");
+            System.out.println("2: tirar carro");
+            Scanner scanner = new Scanner(System.in);
+            int escolha = scanner.nextInt();
+            switch (escolha) {
+                case 1:
+                    colocarCarro();
+                    break;
+                case 2:
+                    tirarCarro();
+                default:
+                    break;
+            }
+            scanner.close();
         }
     }
 
-    public static void colocarCarro() {
-        ModeloDeCarro[] modelo = new ModeloDeCarro[10];
+    public void colocarCarro() {
         for (int i = 0; i < modelo.length; i++) {
-            Scanner scanner = new Scanner(System.in);
             if (modelo[i] == null) {
-                
+                modelo[i] = new ModeloDeCarro(null, null, null);
+                // mew
+                System.out.println("me diga a placa do carro");
+                modelo[i].setPlaca(scanner.nextLine());
+                System.out.println("me diga o modelo do carro");
+                modelo[i].setModelo(scanner.nextLine());
+                System.out.println("me diga a cor do carro");
+                modelo[i].setCor(scanner.nextLine());
                 break;
             }
         }
+        controle = new ControleDeCarro(modelo);
+        // mew
+    }
+
+    public void tirarCarro() {
+        System.out.println(modelo);
     }
 }
