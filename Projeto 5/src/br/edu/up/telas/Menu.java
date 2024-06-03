910fa4a3d889300d05b85d2be6adfada0d34dfb2
lopesmathe <matheuslopesbauer@gmail.com>
2024-06-03 09:12:10 -0300

    arrumando bug

--- Projeto 5/src/br/edu/up/telas/Menu.java
+++ Projeto 5/src/br/edu/up/telas/Menu.java
@@ -14,84 +14,82 @@ public class Menu {
 
     int totalIngressos = 0;
 
-    public void run(){
+    public void run() {
         while (true) {
             System.out.println("1: fazer reserva");
             System.out.println("2: criar evento");
-            System.out.println("3: ver todos os eventos e quantos ingressos foram comprados");            
+            System.out.println("3: ver todos os eventos e quantos ingressos foram comprados");
             System.out.println("4: ver detalhamento dos eventos");
             System.out.println("5: fazer reserva");
-            
 
             Scanner scanner = new Scanner(System.in);
-            
+
             int escolha = scanner.nextInt();
             scanner.nextLine();
             switch (escolha) {
                 case 1:
-                criarEvento();
-                break;
+                    criarEvento();
+                    break;
                 case 2:
-                fazerReserva();
-                break;
+                    fazerReserva();
+                    break;
                 case 3:
-                verNomeDeEventos();
-                break;
+                    verNomeDeEventos();
+                    break;
                 case 4:
-                verDetalhesDeEventos();
-                break;
+                    verDetalhesDeEventos();
+                    break;
                 case 5:
-                fazerReserva();
-                break;
-                case 6:
-                verTodos();
-                break;
+                    fazerReserva();
+                    break;
                 default:
-                break;
+                    break;
             }
         }
     }
 
-    public void criarEvento(){            
-                Prompt.imprimir("me diga qual é o nome do evento");
-                String nome = Prompt.lerLinha();
-                Prompt.imprimir("me diga a data do evento no modelo 00/00/0000");
-                String data = Prompt.lerLinha();
-                Prompt.imprimir("me diga o local onde o evento vai acontecer (bairro, rua, numero)");
-                String local = Prompt.lerLinha();
-                Prompt.imprimir("qual sera a capacidade maxima do lcoal?");
-                int capacidadeMax = Prompt.lerInteiro();
-                Prompt.imprimir("qual sera o valor unitario do ingresso");
-                double valorIngresso = Prompt.lerDecimal();
-                Evento novoevento = new Evento(nome, data, local, capacidadeMax, totalIngressos, valorIngresso);
-                eventos.add(novoevento);
+    public void criarEvento() {
+        Prompt.imprimir("me diga qual é o nome do evento");
+        String nome = Prompt.lerLinha();
+        Prompt.imprimir("me diga a data do evento no modelo 00/00/0000");
+        String data = Prompt.lerLinha();
+        Prompt.imprimir("me diga o local onde o evento vai acontecer (bairro, rua, numero)");
+        String local = Prompt.lerLinha();
+        Prompt.imprimir("qual sera a capacidade maxima do lcoal?");
+        int capacidadeMax = Prompt.lerInteiro();
+        Prompt.imprimir("qual sera o valor unitario do ingresso");
+        double valorIngresso = Prompt.lerDecimal();
+        Evento novoevento = new Evento(nome, data, local, capacidadeMax, totalIngressos, valorIngresso);
+        eventos.add(novoevento);
     }
 
-    public void verNomeDeEventos(){
-        for(Evento evento : eventos){
-            System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    public void verNomeDeEventos() {
+        for (Evento evento : eventos) {
+            System.out.println(
+                    "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
             String detalhesEvento = "Nome: " + evento.getNome() + "    ingressos: " + totalIngressos;
             System.out.println(detalhesEvento);
         }
-        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "-----------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
-    public void verDetalhesDeEventos(){
-        for(Evento evento : eventos){
-            System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+    public void verDetalhesDeEventos() {
+        for (Evento evento : eventos) {
+            System.out.println(
+                    "-----------------------------------------------------------------------------------------------------------------------------------------------------------");
             String detalhesEvento = "Nome: " + evento.getNome() +
-                                    ", Data: " + evento.getData() +
-                                    ", Local: " + evento.getLocal() +
-                                    ", Capacidade máxima: " + evento.getLotacaoMaxima() +
-                                    ", Valor ingresso: " + evento.getPrecoIngresso();
+                    ", Data: " + evento.getData() +
+                    ", Local: " + evento.getLocal() +
+                    ", Capacidade máxima: " + evento.getLotacaoMaxima() +
+                    ", Valor ingresso: " + evento.getPrecoIngresso();
             System.out.println(detalhesEvento);
         }
-        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "-----------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
-    
 
-
-    public void fazerReserva(){
+    public void fazerReserva() {
         System.out.println("me diga o nome do evento");
         String nomeEvento = Prompt.lerLinha();
         System.out.println("me diga seu nome");
@@ -101,7 +99,7 @@ public class Menu {
         System.out.println("me diga a hora de chegada");
         String hora = Prompt.lerLinha();
         Evento eventoSelecionado = null;
-        totalIngressos =+ quantidade; 
+        totalIngressos = +quantidade;
         for (Evento evento : eventos) {
             if (evento.getNome().equalsIgnoreCase(nomeEvento)) {
                 eventoSelecionado = evento;
@@ -113,56 +111,44 @@ public class Menu {
             System.out.println("Evento não encontrado.");
             return;
         }
-        
+
         double valor = eventoSelecionado.getPrecoIngresso() * quantidade;
         System.out.println("o valor total é " + valor);
 
         Pessoa novaPessoa = new Pessoa(nome, quantidade, hora, valor);
         pessoa.add(novaPessoa);
     }
-
-    public void verTodos(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("me diga o nome do evento para ver todos os participantes");
-        String nome = scan.nextLine();
-        Evento getevento = null;
-        for (Evento evento: eventos){
-            if(evento.getNome().equalsIgnoreCase(nome)){
-                getevento = evento;
-                for (Evento pessoasEvento: eventos) {
-                        if () {
-                            
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
-/*public void verTodos(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("me diga o nome do evento para ver todos os participantes");
-        String nomeEvento = scan.nextLine();
-        Evento eventoSelecionado = null;
-
-        for (Evento evento : eventos) {
-            if (evento.getNome().equalsIgnoreCase(nomeEvento)) {
-                eventoSelecionado = evento;
-                break;
-            }
-        }
-
-        if (eventoSelecionado == null) {
-            System.out.println("Evento não encontrado.");
-            return;
-        }
-
-        System.out.println("Lista de participantes do evento " + eventoSelecionado.getNome() + ":");
-        for (Pessoa p : pessoa) {
-            // Assumindo que a Pessoa tem um método getEvento() para pegar o evento associado
-            if (p.getEvento().equals(eventoSelecionado)) {
-                System.out.println("Nome: " + p.getNome() + ", Quantidade: " + p.getQuantidade() + ", Hora de chegada: " + p.getHoraChegada());
-            }
-        }
-    } */
+/*
+ * public void verTodos(){
+ * Scanner scan = new Scanner(System.in);
+ * System.out.println("me diga o nome do evento para ver todos os participantes"
+ * );
+ * String nomeEvento = scan.nextLine();
+ * Evento eventoSelecionado = null;
+ * 
+ * for (Evento evento : eventos) {
+ * if (evento.getNome().equalsIgnoreCase(nomeEvento)) {
+ * eventoSelecionado = evento;
+ * break;
+ * }
+ * }
+ * 
+ * if (eventoSelecionado == null) {
+ * System.out.println("Evento não encontrado.");
+ * return;
+ * }
+ * 
+ * System.out.println("Lista de participantes do evento " +
+ * eventoSelecionado.getNome() + ":");
+ * for (Pessoa p : pessoa) {
+ * // Assumindo que a Pessoa tem um método getEvento() para pegar o evento
+ * associado
+ * if (p.getEvento().equals(eventoSelecionado)) {
+ * System.out.println("Nome: " + p.getNome() + ", Quantidade: " +
+ * p.getQuantidade() + ", Hora de chegada: " + p.getHoraChegada());
+ * }
+ * }
+ * }
+ */
